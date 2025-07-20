4fb80fd56a9401b818ad867d883426cc28d1605f
Amar kumar singh <amar@ostrojs.com>
2025-07-20 21:41:59 +0530

    handle static data like image, css, txt etc

--- stream/response.js
+++ stream/response.js
@@ -7,6 +7,9 @@ class Respose extends Writable {
     this.chunks = [];
     this.statusCode = 200;
     this.headers = {};
+    this.finished = false;
+    this.headersSent = false;
+    this.$isBase64Encoded = true;
     this._resolve = resolve;
   }
 
@@ -15,52 +18,79 @@ class Respose extends Writable {
     callback();
   }
 
+  isBase64Encoded() {
+    return this.$isBase64Encoded
+  }
+  encode(endcodin) {
+    if (endcodin && endcodin.toLowerCase() === 'base64') {
+      this.$isBase64Encoded = true;
+    } else {
+      this.$isBase64Encoded = false;
+    }
+    return this;
+  }
+
+  write(chunk, encoding, callback) {
+    this._implicitHeader();
+    return super.write(chunk, encoding, callback);
+  }
+  end(chunk, encoding, callback) {
+    if (chunk) this.write(chunk, encoding, () => { });
+
+    const buffer = Buffer.concat(this.chunks);
+
+    let isBase64Encoded = this.isBase64Encoded();
+
+    this.finished = true;
+
+    this._resolve({
+      statusCode: this.statusCode,
+      headers: normalizeHeaders(this.headers),
+      body: isBase64Encoded ? buffer.toString('base64') : buffer.toString('utf8'),
+      isBase64Encoded,
+    });
+
+    if (typeof callback === 'function') callback();
+  }
+
+
+
+
   writeHead(statusCode, headers) {
     this.statusCode = statusCode;
+    this._implicitHeader();
 
     if (headers) {
-      let parsedHeaders = {};
-
-      if (typeof headers === 'string') {
-        // Parse headers string into key-value pairs
-        headers.split('\n').forEach(line => {
-          const [key, ...rest] = line.split(':');
-          if (key && rest.length > 0) {
-            parsedHeaders[key.trim()] = rest.join(':').trim();
-          }
-        });
-      } else if (typeof headers === 'object') {
-        parsedHeaders = headers;
+      if (typeof headers === 'object') {
+        this.headers = { ...this.headers, ...headers };
       }
-
-      this.headers = { ...this.headers, ...parsedHeaders };
     }
   }
+
   setHeader(key, value) {
-    this.headers[key] = value;
+    this.headers[key.toLowerCase()] = value;
+  }
+
+  getHeader(key) {
+    return this.headers[key.toLowerCase()];
   }
 
   getHeaders() {
     return this.headers;
   }
 
-  getHeader(key) {
-    return this.headers[key];
+  hasHeader(key) {
+    return key.toLowerCase() in this.headers;
+  }
+
+  removeHeader(key) {
+    delete this.headers[key.toLowerCase()];
   }
 
   _implicitHeader() {
     if (!this.headersSent) {
       this.headersSent = true;
     }
-  }
-  end(chunk) {
-    if (chunk) this.write(chunk, null, () => { });
-    const body = Buffer.concat(this.chunks).toString();
-    this._resolve({
-      statusCode: this.statusCode,
-      headers: normalizeHeaders(this.headers),
-      body,
-    });
   }
 }
 
